9a7c8b35956cb3bb41846c09482ccc703e976c63
Aleksandr Losev <cahektpx1990@gmail.com>
2021-05-14 03:31:08 +0300

    added section income and made adaptive

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,6 @@
-let header = document.querySelector(".header");
+// scroll header
+
+const header = document.querySelector(".header");
 
 document.addEventListener("scroll", () => {
   if (document.documentElement.scrollTop > 1) {
@@ -7,3 +9,14 @@ document.addEventListener("scroll", () => {
     header.classList.remove("header_has-bg");
   }
 });
+
+// mobile menu 
+
+const burgerEl = document.querySelector('.burger');
+const menuContainerEl =document.querySelector('.header__menu-container');
+
+burgerEl.addEventListener('click', function() {
+  this.classList.toggle('burger_visible');
+  menuContainerEl.classList.toggle('header__menu-container_visible');
+  document.body.classList.toggle('content-hidden');
+})
