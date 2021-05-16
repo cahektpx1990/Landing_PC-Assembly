9bdc30adb3a0de7ac773860ff33fc7d61d8e6d5c
Aleksandr Losev <cahektpx1990@gmail.com>
2021-05-17 01:50:56 +0300

    added changes in package.json

---
+++ assets/js/components/mobileMenu/index.js
@@ -0,0 +1,13 @@
+// mobile menu 
+
+(function(){
+
+  const burgerEl = document.querySelector('.burger');
+  const menuContainerEl =document.querySelector('.header__menu-container');
+  
+  burgerEl.addEventListener('click', function() {
+    this.classList.toggle('burger_visible');
+    menuContainerEl.classList.toggle('header__menu-container_visible');
+    document.body.classList.toggle('content-hidden');
+  });
+})
